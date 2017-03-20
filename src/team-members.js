4880b6d38767343555a86f23dd2c86298cdeeb0b
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-03-19 18:35:53 -0700

    Add TeamMember bio component
    Extract hard-coded team members info

---
+++ src/team-members.js
@@ -0,0 +1,20 @@
+const teamMembers = [
+  {
+    name: 'Kyle Corbelli',
+    titles: [
+      'Cofounder',
+      'Web Developer',
+      'Software Engineer'
+    ]
+  },
+  {
+    name: 'Stephen Hundt',
+    titles: [
+      'Cofounder',
+      'Hydrologist',
+      'Software Engineer'
+    ]
+  }
+];
+
+export default teamMembers;
